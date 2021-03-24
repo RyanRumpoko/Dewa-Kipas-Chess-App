f0656f4ec3e25547397e2e1978daa7bf8d2532ea
RyanRumpokocd..clear <xiipa4ryanrumpoko@gmail.com>
2021-03-24 07:16:54 +0700

    Styling Home Standup Pagi

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,16 +1,16 @@
-import { useState } from "react";
+// import { useState } from "react";
 import { useHistory } from "react-router-dom";
-import Button from 'react-bootstrap/Button'
+// import Button from "react-bootstrap/Button";
 
 export default function Nav() {
-  const [color, setColor] = useState("btn btn-outline-dark");
-  function changeColor(e) {
-    if (e === "over") {
-      setColor("btn btn-outline-warning");
-    } else {
-      setColor("btn btn-outline-dark");
-    }
-  }
+  // const [color, setColor] = useState("btn btn-outline-dark");
+  // function changeColor(e) {
+  //   if (e === "over") {
+  //     setColor("btn btn-outline-warning");
+  //   } else {
+  //     setColor("btn btn-outline-dark");
+  //   }
+  // }
   const history = useHistory();
 
   async function logout() {
@@ -18,21 +18,36 @@ export default function Nav() {
     await history.push("/login");
   }
   return (
-    <div
-      className="col-12"
-      style={{ display: "flex", justifyContent: "flex-end", backgroundColor: "#161512" }}
-    >
-      <Button
-        variant="secondary"
-        className={color}
-        style={{backgroundColor: "#161512"}}
-        // onMouseOver={() => changeColor("over")}
-        // onMouseLeave={() => changeColor("leave")}
+    // <div
+    //   className="col-12"
+    //   style={{
+    //     display: "flex",
+    //     justifyContent: "flex-end",
+    //     backgroundColor: "#161512",
+    //   }}
+    // >
+    //   <Button
+    //     variant="secondary"
+    //     className={color}
+    //     style={{backgroundColor: "#161512"}}
+    //     // onMouseOver={() => changeColor("over")}
+    //     // onMouseLeave={() => changeColor("leave")}
+    //     onClick={() => logout()}
+    //   >
+    //     <i className="fas fa-sign-out-alt" style={{ color: "black" }} />
+    //     <span>&nbsp;Log Out</span>
+    //   </Button>
+    // </div>
+    <nav className="navbar navbar-dark bg-dark">
+      <span className="navbar-brand mb-0 h1">DEWA KIPAS</span>
+      <button
+        className="btn btn-outline-light"
+        style={{ justifyContent: "flex-end" }}
         onClick={() => logout()}
       >
-        <i className="fas fa-sign-out-alt" style={{ color: "black" }} />
+        <i className="fas fa-sign-out-alt" style={{ color: "white" }} />
         <span>&nbsp;Log Out</span>
-      </Button>
-    </div>
+      </button>
+    </nav>
   );
 }
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,9 +1,9 @@
 import { useHistory, useLocation } from "react-router-dom";
-import { useState, useEffect, useCallback } from "react";
+import { useState, useEffect } from "react";
 import Nav from "../components/Nav";
 import axios from "../api/axios";
 import CardHistory from "../components/CardHistory";
-import { socket, ENDPOINT } from "../connections/socketio.js";
+import { socket } from "../connections/socketio.js";
 
 export default function Home() {
   const { state } = useLocation();
@@ -12,10 +12,13 @@ export default function Home() {
   const [openModalCreateRoom, setOpenModalCreateRoom] = useState(false);
   const [inputRoomId, setInputRoomId] = useState("");
   const [histories, setHistories] = useState([]);
+  const [isDropdown, setIsDropdown] = useState(true);
   function vsPlayer() {
     setOpenModalCreateRoom(true);
-    console.log(openModalCreateRoom, "sudah tertoggle");
-    // history.push("/dashboard/player");
+    setIsDropdown(false);
+  }
+  function handleBack() {
+    setIsDropdown(true);
   }
   function createRoom() {
     history.push(`/dashboard/player/new`, state);
@@ -24,15 +27,14 @@ export default function Home() {
     history.push(`/dashboard/player/${inputRoomId}`, state);
   }
   function onChangeInputRoomId(e) {
-    // console.log(e.target.value);
     setInputRoomId(e.target.value);
   }
   function vsBot() {
     history.push("/dashboard/bot", state);
   }
-  function matchmaking () {
-    socket.emit("matchmaking", state)
-    history.push("/dashboard/player/matchmaking", state)
+  function matchmaking() {
+    socket.emit("matchmaking", state);
+    history.push("/dashboard/player/matchmaking", state);
   }
 
   useEffect(() => {
@@ -46,7 +48,6 @@ export default function Home() {
             access_token: localStorage.access_token,
           },
         });
-
         setHistories(data);
       } catch ({ response }) {
         console.log(response.data);
@@ -54,158 +55,280 @@ export default function Home() {
     }
     getHistoryUser();
     return () => ac.abort();
-
   }, [openModalCreateRoom, state]);
+
   return (
-    <div className="container-fluid bg-info">
-      <div className="row">
-        <Nav />
-      </div>
-      {openModalCreateRoom ? (
-        <div className="row">
-          <button className="btn" type="button" onClick={createRoom}>
-            createRoom
-          </button>
-          <>
-            <label>input room id</label>
-            <input type="text" onChange={(e) => onChangeInputRoomId(e)} />
-            <button className="btn" type="button" onClick={joinRoom}>
-              joinRoom
-            </button>
-          </>
-        </div>
-      ) : (
-        <div></div>
-      )}
-      <div className="row" style={{ height: "100vh" }}>
-        <div className="col-6 bg-warning">
-          <h1 className="text-center">Want to play now?</h1>
-          <div className="row">
+    <>
+      <Nav />
+      <div className="container-fluid">
+        <div className="row justify-content-center">
+          <div className="col-10 col-md-8 col-lg-6 my-3">
             <div
-              className="dropdown"
+              className="card text-center"
               style={{
-                display: "flex",
-                justifyContent: "center",
-                alignItems: "center",
+                height: "300px",
+                width: "800px",
+                backgroundColor: "#262421",
               }}
             >
-              <button
-                className="btn btn-outline-primary dropdown-toggle"
-                type="button"
-                id="dropdownMenuButton1"
-                data-bs-toggle="dropdown"
-                aria-expanded="false"
-                style={{ height: "100px", width: "100px" }}
-              >
-                Play
-              </button>
-              <ul
-                className="dropdown-menu"
-                aria-labelledby="dropdownMenuButton1"
-              >
-                <li>
-                  <button className=" dropdown-item" onClick={vsPlayer}>
-                    <i className="fas fa-user"> V.S. Player</i>
+              <div className="card-header text-white h1">Want to play now?</div>
+              {isDropdown ? (
+                <div
+                  className="dropdown mt-5"
+                  style={{
+                    display: "flex",
+                    justifyContent: "center",
+                    alignItems: "center",
+                  }}
+                >
+                  <button
+                    className="btn btn-outline-light dropdown-toggle"
+                    type="button"
+                    id="dropdownMenuButton1"
+                    data-bs-toggle="dropdown"
+                    aria-expanded="false"
+                    style={{ height: "100px", width: "100px" }}
+                  >
+                    Play
                   </button>
-                </li>
-                <li>
-                  <button className=" dropdown-item" onClick={vsBot}>
-                    <i className="fas fa-robot"> V.S. Bot</i>
-                  </button>
-                </li>
-                <li>
-                  <button className=" dropdown-item" onClick={matchmaking}>
-                    <i className="fas fa-robot"> Matchmaking</i>
-                  </button>
-                </li>
-              </ul>
+                  <ul
+                    className="dropdown-menu"
+                    aria-labelledby="dropdownMenuButton1"
+                  >
+                    <li>
+                      <button className=" dropdown-item" onClick={vsPlayer}>
+                        <i className="fas fa-user"> V.S. Player</i>
+                      </button>
+                    </li>
+                    <li>
+                      <button className=" dropdown-item" onClick={vsBot}>
+                        <i className="fas fa-robot"> V.S. Bot</i>
+                      </button>
+                    </li>
+                    <li>
+                      <button className=" dropdown-item" onClick={matchmaking}>
+                        <i className="fas fa-robot"> Matchmaking</i>
+                      </button>
+                    </li>
+                  </ul>
+                </div>
+              ) : (
+                <>
+                  <div className="row justify-content-center">
+                    <div className="col-4 my-3">
+                      <button
+                        className="btn btn-outline-light"
+                        type="button"
+                        onClick={createRoom}
+                      >
+                        Create Room
+                      </button>
+                    </div>
+                  </div>
+                  <div className="row justify-content-center">
+                    <div className="col-8 my-3">
+                      <label className="text-white mr-2">Input Room ID</label>
+                      <input
+                        type="text"
+                        onChange={(e) => onChangeInputRoomId(e)}
+                      />
+                      <button
+                        className="btn btn-outline-light ml-2"
+                        type="button"
+                        onClick={joinRoom}
+                      >
+                        Join Room
+                      </button>
+                    </div>
+                  </div>
+                  <div className="row justify-content-center">
+                    <div className="col-4 my-3">
+                      <button
+                        className="btn btn-outline-light"
+                        type="button"
+                        onClick={handleBack}
+                      >
+                        Back
+                      </button>
+                    </div>
+                  </div>
+                </>
+              )}
             </div>
           </div>
-          <div className="row">{JSON.stringify(histories, null, 2)}</div>
-        </div>
-        <div className="col-6 bg-danger">
-          <div className="row">
+          <div className="col-10 col-md-8 col-lg-3 my-3">
             <div
-              className="col-2"
-              style={{ display: "flex", justifyContent: "center" }}
+              className="card"
+              style={{
+                height: "150px",
+                width: "450px",
+                backgroundColor: "#262421",
+              }}
             >
-              <img
-                src={state.pictureUrl}
-                className="img-thumbnail"
-                height="100"
-                width="100"
-                alt=""
-              />
-            </div>
-            <div className="col-8">
-              <div className="row">
-                <span>
-                  Name<h3>{state ? state.username : ""}</h3>
-                </span>
+              <div className="d-flex justify-content-around">
+                <div className="col-4 my-3">
+                  <img
+                    src={state.pictureUrl}
+                    className="img-thumbnail"
+                    alt=""
+                  />
+                </div>
+                <div className="col-8 my-3">
+                  <h3 className="text-white">{state.username}</h3>
+                  <h3 className="text-white">Score : {state.eloRating}</h3>
+                </div>
               </div>
             </div>
           </div>
-          <div className="row" style={{ height: "50vh" }}>
-            <h1 className="text-center">Leaderboard</h1>
-          </div>
-          <div className="row" style={{ height: "50vh" }}>
-            <div className="col">
-              <h1 className="text-center">History</h1>
-              <div
-                tabIndex="0"
-                style={{
-                  overflowY: "scroll",
-                  minHeight: "100x",
-                  maxHeight: "220px",
-                  border: "5px solid black",
-                }}
-                className="row"
-              >
-                {histories
-                  ? histories.map((history, i) => (
-                      <CardHistory history={history} key={`data ke${i + 1}`} />
-                    ))
-                  : null}
-                {/* <div className="card mb-3" id="2">
-                  <h5 className="card-header">Player One</h5>
-                  <div className="card-body">
-                    <p>
-                      Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                      Asperiores totam molestiae id? Similique nemo a alias
-                      quasi corporis rem pariatur. Fugit ratione corrupti,
-                      facere nesciunt praesentium vel asperiores. Vero, velit.
-                    </p>
-                  </div>
-                </div>
-                <div className="card mb-3" id="3">
-                  <h5 className="card-header">Player One</h5>
-                  <div className="card-body">
-                    <p>
-                      Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                      Asperiores totam molestiae id? Similique nemo a alias
-                      quasi corporis rem pariatur. Fugit ratione corrupti,
-                      facere nesciunt praesentium vel asperiores. Vero, velit.
-                    </p>
-                  </div>
-                </div>
-                <div className="card mb-3" id="4">
-                  <h5 className="card-header">Player One</h5>
-                  <div className="card-body">
-                    <p>
-                      Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                      Asperiores totam molestiae id? Similique nemo a alias
-                      quasi corporis rem pariatur. Fugit ratione corrupti,
-                      facere nesciunt praesentium vel asperiores. Vero, velit.
-                    </p>
-                  </div>
-                </div> */}
-
-                {/*  */}
-              </div>
+        </div>
+        <div className="row justify-content-center">
+          <div className="col-10 col-md-8 col-lg-3 my-3">
+            <div
+              className="card text-center"
+              style={{
+                height: "300px",
+                width: "350px",
+                backgroundColor: "#262421",
+              }}
+            >
+              <div className="card-header text-white h1">Leaderboard</div>
             </div>
           </div>
+          <div className="col-10 col-md-8 col-lg-3 my-3">
+            <div
+              className="card text-center"
+              style={{
+                height: "300px",
+                width: "350px",
+                backgroundColor: "#262421",
+              }}
+            >
+              <div className="card-header text-white h1">History</div>
+              {histories
+                ? histories.map((history, i) => (
+                    <CardHistory history={history} key={`data ke${i + 1}`} />
+                  ))
+                : null}
+            </div>
+          </div>
+          <div className="col-10 col-md-8 col-lg-3 my-3"></div>
         </div>
       </div>
-    </div>
+      {/* <div className="container-fluid">
+        {openModalCreateRoom ? (
+          <div className="row">
+            <button className="btn" type="button" onClick={createRoom}>
+              createRoom
+            </button>
+            <>
+              <label>input room id</label>
+              <input type="text" onChange={(e) => onChangeInputRoomId(e)} />
+              <button className="btn" type="button" onClick={joinRoom}>
+                joinRoom
+              </button>
+            </>
+          </div>
+        ) : (
+          <div></div>
+        )}
+        <div className="row" style={{ height: "100vh" }}>
+          <div className="col-6 bg-warning">
+            <h1 className="text-center">Want to play now?</h1>
+            <div className="row">
+              <div
+                className="dropdown"
+                style={{
+                  display: "flex",
+                  justifyContent: "center",
+                  alignItems: "center",
+                }}
+              >
+                <button
+                  className="btn btn-outline-primary dropdown-toggle"
+                  type="button"
+                  id="dropdownMenuButton1"
+                  data-bs-toggle="dropdown"
+                  aria-expanded="false"
+                  style={{ height: "100px", width: "100px" }}
+                >
+                  Play
+                </button>
+                <ul
+                  className="dropdown-menu"
+                  aria-labelledby="dropdownMenuButton1"
+                >
+                  <li>
+                    <button className=" dropdown-item" onClick={vsPlayer}>
+                      <i className="fas fa-user"> V.S. Player</i>
+                    </button>
+                  </li>
+                  <li>
+                    <button className=" dropdown-item" onClick={vsBot}>
+                      <i className="fas fa-robot"> V.S. Bot</i>
+                    </button>
+                  </li>
+                  <li>
+                    <button className=" dropdown-item" onClick={matchmaking}>
+                      <i className="fas fa-robot"> Matchmaking</i>
+                    </button>
+                  </li>
+                </ul>
+              </div>
+            </div>
+            <div className="row">{JSON.stringify(histories, null, 2)}</div>
+          </div>
+          <div className="col-6 bg-danger">
+            <div className="row">
+              <div
+                className="col-2"
+                style={{ display: "flex", justifyContent: "center" }}
+              >
+                <img
+                  src={state.pictureUrl}
+                  className="img-thumbnail"
+                  height="100"
+                  width="100"
+                  alt=""
+                />
+              </div>
+              <div className="col-8">
+                <div className="row">
+                  <span>
+                    Name<h3>{state ? state.username : ""}</h3>
+                  </span>
+                </div>
+              </div>
+            </div>
+            <div className="row" style={{ height: "50vh" }}>
+              <h1 className="text-center">Leaderboard</h1>
+            </div>
+            <div className="row" style={{ height: "50vh" }}>
+              <div className="col">
+                <h1 className="text-center">History</h1>
+                <div
+                  tabIndex="0"
+                  style={{
+                    overflowY: "scroll",
+                    minHeight: "100x",
+                    maxHeight: "220px",
+                    border: "5px solid black",
+                  }}
+                  className="row"
+                >
+                  {histories
+                    ? histories.map((history, i) => (
+                        <CardHistory
+                          history={history}
+                          key={`data ke${i + 1}`}
+                        />
+                      ))
+                    : null}
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div> */}
+    </>
   );
 }
