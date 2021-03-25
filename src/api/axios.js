338c38827f08a06037f54af6ebfb85ff36f9eac3
Shulthan Daffa'ul Iman <shulthan.daffaul.iman@gmail.com>
2021-03-25 11:19:08 +0700

    prepare to deploy

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,6 +1,9 @@
 import axios from "axios";
+// const instance = axios.create({
+//   baseURL: "http://localhost:4000/",
+// });
 const instance = axios.create({
-  baseURL: "http://localhost:4000/",
+  baseURL: "https://server-chess-dewa-kipas.herokuapp.com/",
 });
 
 export default instance;
